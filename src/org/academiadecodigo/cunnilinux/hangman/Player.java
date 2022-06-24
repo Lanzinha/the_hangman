7dba6d017b7d7a7294aff2b8bdd3eb11934ab346
lanzinha <codecadet@musk.local>
2022-06-24 19:58:32 +0000

    add broadcast

--- src/org/academiadecodigo/cunnilinux/hangman/Player.java
+++ src/org/academiadecodigo/cunnilinux/hangman/Player.java
@@ -1,9 +1,6 @@
 package org.academiadecodigo.cunnilinux.hangman;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.logging.Level;
@@ -73,3 +70,133 @@ public class Player implements Runnable {
 
 
 }
+
+
+
+package org.academiadecodigo.cunnilinux;
+
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.io.PrintWriter;
+        import java.net.InetAddress;
+        import java.net.Socket;
+
+
+public class ChatClient implements Runnable {
+    private Socket clientSocket;
+    //private final int portNumber;
+    private PrintWriter out;
+    private BufferedReader terminalIn;
+    private BufferedReader in;
+    private final ChatServer server;
+    //BufferedReader in;
+
+
+    public ChatClient(Socket clientSocket, ChatServer server) {
+
+        this.clientSocket = clientSocket;
+        this.server = server;
+        //receiveMessage();
+        openStreams();
+        sendMessage();
+   /*     try {
+            //link();
+            start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+*/
+
+    }
+
+    public static void main(String[] args) {
+        new ChatClient(new Socket(), new ChatServer(9000));
+
+    }
+/*
+
+    private void link() {
+        try {
+            clientSocket = new Socket(InetAddress.getLocalHost(),);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+*/
+
+
+    private void openStreams() {
+        terminalIn = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void receiveMessage(String message) {
+
+        out.println(message);
+
+
+    }
+
+    public synchronized void sendMessage() {
+        out.println("Welcome my dear friend. How can i assist u?");
+        String lanzas = null;
+        try {
+            out.println("pls write your name");
+            lanzas = in.readLine();
+            // while (true) {
+            String message = in.readLine();
+            out.println(lanzas + ": " + message);
+            System.out.println(message);
+
+            //}
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+   /* private void start() throws IOException {
+        System.out.println(serverReply.readLine());
+        out.println(terminalIn.readLine());
+        System.out.println("Start your chat:  ");
+        while (true) {
+            String message = terminalIn.readLine();
+            if (!message.equalsIgnoreCase("Quit")) {
+
+                out.println(message);
+                System.out.println(serverReply.readLine());
+            } else {
+                break;
+            }
+        }
+        clientSocket.close();
+    }*/
+
+
+    @Override
+    public void run() {
+
+        try {
+            BufferedReader in = null;
+            in = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            while (!this.clientSocket.isClosed()) {
+                String message = in.readLine();
+                //if (in == null) {
+                //server.clientLeave(this.clientSocket);
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+
+    }
+}
+
--- src/org/academiadecodigo/cunnilinux/hangman/Server.java
+++ src/org/academiadecodigo/cunnilinux/hangman/Server.java
@@ -3,6 +3,7 @@ package org.academiadecodigo.cunnilinux.hangman;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
@@ -67,3 +68,89 @@ public class Server {
 
 
 }
+
+
+package org.academiadecodigo.cunnilinux;
+
+        import jdk.nashorn.internal.ir.WhileNode;
+
+        import java.io.*;
+        import java.net.ServerSocket;
+        import java.net.Socket;
+        import java.util.LinkedList;
+        import java.util.Vector;
+        import java.util.concurrent.CopyOnWriteArrayList;
+        import java.util.concurrent.ExecutorService;
+        import java.util.concurrent.Executors;
+
+public class ChatServer {
+
+    private final ExecutorService fixedPool;
+    private CopyOnWriteArrayList<ChatClient> clientList;
+    private static int portNumber;
+    ServerSocket serverSocket;
+
+
+    public ChatServer(int portNumber) {
+        clientList = new CopyOnWriteArrayList<>();
+        fixedPool = Executors.newFixedThreadPool(3);
+        ChatServer.portNumber = portNumber;
+        start();
+
+
+    }
+
+
+    public static void main(String[] args) {
+        new ChatServer(9000);
+
+    }
+
+
+    public void start() {
+
+        ServerSocket serverSocket = null;
+        try {
+
+            serverSocket = new ServerSocket(portNumber);
+            while (true) {
+
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Connection Established");
+                ChatClient chatClient = new ChatClient(clientSocket, this);
+                clientList.add(chatClient);
+                fixedPool.submit(chatClient);
+                broadCast("hello");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+    /*public void clientLeave(Socket clientSocket) {
+        broadCast(clientSocket + "left the chat!!!", clientSocket);
+        this.clientList.remove(clientSocket);
+    }
+*/
+    private synchronized void broadCast(String message) {
+        for (ChatClient chatClient : clientList) {
+            chatClient.receiveMessage(message);
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
