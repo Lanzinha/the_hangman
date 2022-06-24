4d672d79b7237dc7083ea79368282185cb9881fe
@tpr09756 <tpr09756@gmail.com>
2022-06-24 16:44:06 +0000

    add server

--- src/org/academiadecodigo/cunnilinux/hangman/Main.java
+++ src/org/academiadecodigo/cunnilinux/hangman/Main.java
@@ -3,7 +3,20 @@ package org.academiadecodigo.cunnilinux.hangman;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("teste");
+        try {
+
+            int port = args.length > 0 ? Integer.parseInt(args[0]) : Server.DEFAULT_PORT;
+
+            Server server = new Server();
+            server.listen(port);
+
+        } catch (NumberFormatException e) {
+
+            System.err.println("Usage: WebServer [PORT]");
+            System.exit(1);
+
+        }
+
 
     }
 }
--- src/org/academiadecodigo/cunnilinux/hangman/Server.java
+++ src/org/academiadecodigo/cunnilinux/hangman/Server.java
@@ -1,4 +1,82 @@
 package org.academiadecodigo.cunnilinux.hangman;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Server {
+
+    private static final Logger logger = Logger.getLogger(Server.class.getName());
+
+    public static final int DEFAULT_PORT = 8080;
+
+    private ServerSocket serverSocket = null;
+    private Socket playerSocket;
+    private Player playerDispatcher;
+    private BufferedReader inputBufferedReader;
+    private BufferedWriter outputBufferedWriter;
+
+
+    public void listen(int port) {
+
+        ExecutorService playersPool = Executors.newFixedThreadPool(3);
+        try {
+            serverSocket = new ServerSocket(port);
+
+            while (true) {
+                playerSocket = serverSocket.accept();
+                //playerDispatcher = new Player(playerSocket);
+                inputBufferedReader = new BufferedReader(new InputStreamReader(playerSocket.getInputStream()));
+                outputBufferedWriter = new BufferedWriter(new OutputStreamWriter(playerSocket.getOutputStream()));
+
+                logger.log(Level.INFO, "server bind to " + getAddress());
+
+                //playersPool.submit(playerDispatcher);
+
+
+                String line = inputBufferedReader.readLine();
+                outputBufferedWriter.write(line);
+                System.out.println(line);
+
+            }
+        } catch (IOException e) {
+
+            logger.log(Level.SEVERE, "could not bind to port " + port);
+            logger.log(Level.SEVERE, e.getMessage());
+            System.exit(1);
+        }
+    }
+
+
+
+    private String getAddress() {
+
+        if (serverSocket == null) {
+            return null;
+        }
+
+        return serverSocket.getInetAddress().getHostAddress() + ":" + serverSocket.getLocalPort();
+    }
+
+    public class PlayerConnection implements Runnable{
+
+
+        Server webServer;
+        Player playerDispatcher;
+
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+
+
+
+
 }
