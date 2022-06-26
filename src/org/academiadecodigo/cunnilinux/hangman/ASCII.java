bc1bbe00e8f486fbfd7d62740ad74ca099f9b877
carlos_regis <zeca2078@gmail.com>
2022-06-26 21:19:25 +0000

    add visual effects

--- src/org/academiadecodigo/cunnilinux/hangman/ASCII.java
+++ src/org/academiadecodigo/cunnilinux/hangman/ASCII.java
@@ -2,6 +2,7 @@ package org.academiadecodigo.cunnilinux.hangman;
 
 public class ASCII {
 
+    public static final String ANSI_CYAN = "\u001B[36m";
     public static String GAME_LOGO = new String(ANSI_CYAN + "\n" +
             " .----------------.  .----------------.  .-----------------. .----------------.  .----------------.  .----------------.  .-----------------.\n" +
             "| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |\n" +
