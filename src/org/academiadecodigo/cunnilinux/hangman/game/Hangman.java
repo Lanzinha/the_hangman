bfeb9e7ee73ca0f75fb4ec2450b5f105a3fd30ee
carlos_regis <zeca2078@gmail.com>
2022-07-07 12:21:29 +0000

    refactor Hangman class

--- src/org/academiadecodigo/cunnilinux/hangman/game/Hangman.java
+++ src/org/academiadecodigo/cunnilinux/hangman/game/Hangman.java
@@ -4,85 +4,34 @@ import org.academiadecodigo.cunnilinux.hangman.ui.ConsoleColor;
 
 public class Hangman {
     private static volatile int lives = 6;
-    private String imgHangman =
-            " _____\n" +
-            " |/\n" +
-            " |\n" +
-            " |\n" +
-            " |\n" +
-            " |\n" +
-            " |\n" +
-            "========\n";
+    private String imgHangman = " _____\n" + " |/\n" + " |\n" + " |\n" + " |\n" + " |\n" + " |\n" + "========\n";
 
     //take a life and update the hangman image
     public synchronized void next() {
 
         switch (--lives) {
             case 0:
-                imgHangman = " _____\n" +
-                        " |/  |\n" +
-                        " |   0\n" +
-                        " |  -O-\n" +
-                        " |   \" \n" +
-                        " |\n" +
-                        " |\n" +
-
-                        "========\n";
+                imgHangman = " _____\n" + " |/  |\n" + " |   0\n" + " |  -O-\n" + " |   \" \n" + " |\n" + " |\n" + "========\n";
                 break;
 
             case 1:
-                imgHangman = " _____\n" +
-                        " |/  |\n" +
-                        " |   0\n" +
-                        " |  -O-\n" +
-                        " |\n" +
-                        " |\n" +
-                        " |\n" +
-                        "========\n";
+                imgHangman = " _____\n" + " |/  |\n" + " |   0\n" + " |  -O-\n" + " |\n" + " |\n" + " |\n" + "========\n";
                 break;
 
             case 2:
-                imgHangman = " _____\n" +
-                        " |/  |\n" +
-                        " |   0\n" +
-                        " |  -O\n" +
-                        " |\n" +
-                        " |\n" +
-                        " |\n" +
-                        "========\n";
+                imgHangman = " _____\n" + " |/  |\n" + " |   0\n" + " |  -O\n" + " |\n" + " |\n" + " |\n" + "========\n";
                 break;
 
             case 3:
-                imgHangman = " _____\n" +
-                        " |/  |\n" +
-                        " |   0\n" +
-                        " |   O\n" +
-                        " |\n" +
-                        " |\n" +
-                        " |\n" +
-                        "========\n";
+                imgHangman = " _____\n" +" |/  |\n" + " |   0\n" + " |   O\n" + " |\n" + " |\n" + " |\n" + "========\n";
                 break;
 
             case 4:
-                imgHangman = " _____\n" +
-                        " |/  |\n" +
-                        " |   0\n" +
-                        " |\n" +
-                        " |\n" +
-                        " |\n" +
-                        " |\n" +
-                        "========\n";
+                imgHangman = " _____\n" + " |/  |\n" + " |   0\n" + " |\n" + " |\n" + " |\n" + " |\n" + "========\n";
                 break;
 
             case 5:
-                imgHangman = " _____\n" +
-                        " |/  |\n" +
-                        " |\n" +
-                        " |\n" +
-                        " |\n" +
-                        " |\n" +
-                        " |\n" +
-                        "========\n";
+                imgHangman = " _____\n" + " |/  |\n" + " |\n" + " |\n" + " |\n" + " |\n" + " |\n" + "========\n";
                 break;
         }
     }
