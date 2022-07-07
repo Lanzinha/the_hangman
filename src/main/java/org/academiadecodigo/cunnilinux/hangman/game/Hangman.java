23a2ea1751b4c18ec9933793472c0bf0de7c1c90
carlos_regis <zeca2078@gmail.com>
2022-07-07 14:50:18 +0000

    add maven config

--- src/main/java/org/academiadecodigo/cunnilinux/hangman/game/Hangman.java
+++ src/main/java/org/academiadecodigo/cunnilinux/hangman/game/Hangman.java
@@ -23,7 +23,7 @@ public class Hangman {
                 break;
 
             case 3:
-                imgHangman = " _____\n" +" |/  |\n" + " |   0\n" + " |   O\n" + " |\n" + " |\n" + " |\n" + "========\n";
+                imgHangman = " _____\n" + " |/  |\n" + " |   0\n" + " |   O\n" + " |\n" + " |\n" + " |\n" + "========\n";
                 break;
 
             case 4:
@@ -46,5 +46,26 @@ public class Hangman {
 
         return lives == 0;
 
+    }
+
+    public enum HangmanImage {
+
+        ZERO(0, " _____\n" + " |/  |\n" + " |   0\n" + " |  -O-\n" + " |   \" \n" + " |\n" + " |\n" + "========\n"),
+        ONE(1, " _____\n" + " |/  |\n" + " |   0\n" + " |  -O-\n" + " |\n" + " |\n" + " |\n" + "========\n"),
+        TWO(2, " _____\n" + " |/  |\n" + " |   0\n" + " |  -O\n" + " |\n" + " |\n" + " |\n" + "========\n"),
+        THREE(3, " _____\n" + " |/  |\n" + " |   0\n" + " |   O\n" + " |\n" + " |\n" + " |\n" + "========\n"),
+        FOUR(4, " _____\n" + " |/  |\n" + " |   0\n" + " |\n" + " |\n" + " |\n" + " |\n" + "========\n"),
+        FIVE(5, " _____\n" + " |/  |\n" + " |\n" + " |\n" + " |\n" + " |\n" + " |\n" + "========\n"),
+        SIX(6, " _____\n" + " |/\n" + " |\n" + " |\n" + " |\n" + " |\n" + " |\n" + "========\n");
+
+        private int lives;
+        private String strImage;
+
+        HangmanImage(int lives, String strImage) {
+
+            this.lives = lives;
+            this.strImage = strImage;
+
+        }
     }
 }
